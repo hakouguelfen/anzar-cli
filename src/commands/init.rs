08cdb0e54b45b7d1b578733e7b3d42acdb784d92
Hakou Guelfen <hakoudev@gmail.com>
2026-04-10 16:52:50 +0100

    Feat: Show automatically auth section depending on the strategy (#2)
    Closes #2

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -5,9 +5,12 @@ use owo_colors::OwoColorize;
 
 use crate::{
     dialoger::{cache::select_cache, database::select_database, strategy::select_strategy},
+    models::strategy::AuthStrategy,
     theme::theme,
 };
 
+const JWT_AUTH: &str = include_str!("../templates/auth/jwt.yml");
+const SESSION_AUTH: &str = include_str!("../templates/auth/session.yml");
 const CONFIG_TEMPLATE: &str = include_str!("../templates/configuration.yml");
 const COMPOSE_TEMPLATE: &str = include_str!("../templates/compose.yml");
 
@@ -21,54 +24,71 @@ pub fn run(app_name: Option<String>) {
         std::process::exit(1);
     }
 
-    // Detect existing files
+    build_compose(app_name);
+    build_anzar()
+}
+
+fn build_compose(app_name: Option<String>) {
     if std::path::Path::new("compose.yml").exists() {
         let confirmation = Confirm::new()
             .with_prompt("compose.yml already exists. Overwrite?")
             .interact()
             .unwrap();
 
-        if confirmation {
-            let name: String = match app_name {
-                Some(n) => n,
-                None => dialoguer::Input::with_theme(&theme())
-                    .with_prompt("Your app name?")
-                    .interact_text()
-                    .unwrap(),
-            };
-            println!("Initializing project: {}", name);
-            let compose_content = COMPOSE_TEMPLATE.replace("{{NAME}}", &name);
-
-            match fs::write("compose.yml", compose_content) {
-                Ok(_) => println!("{} {}", "✓ Created".green().bold(), "compose.yml".cyan()),
-                Err(e) => eprintln!("{} {}", "✗ Failed to create file:".red().bold(), e),
-            }
+        if !confirmation {
+            return;
         }
     }
 
+    let name: String = match app_name {
+        Some(n) => n,
+        None => dialoguer::Input::with_theme(&theme())
+            .with_prompt("Your app name?")
+            .interact_text()
+            .unwrap(),
+    };
+    println!("Initializing project: {}", name);
+    let compose_content = COMPOSE_TEMPLATE.replace("{{NAME}}", &name);
+
+    match fs::write("compose.yml", compose_content) {
+        Ok(_) => println!("{} {}", "✓ Created".green().bold(), "compose.yml".cyan()),
+        Err(e) => eprintln!("{} {}", "✗ Failed to create file:".red().bold(), e),
+    }
+}
+
+fn build_anzar() {
     if std::path::Path::new("anzar.yml").exists() {
         let confirmation = Confirm::new()
             .with_prompt("anzar.yml already exists. Overwrite?")
             .interact()
             .unwrap();
 
-        if confirmation {
-            let (db_driver, db_uri) = select_database();
-            let (cache_driver, cache_uri) = select_cache();
-            let strategy = select_strategy();
-
-            let config_content = CONFIG_TEMPLATE
-                .replace("{{DATABASE_DRIVER}}", &db_driver.to_string())
-                .replace("{{DATABASE_URI}}", &db_uri)
-                .replace("{{CACHE_DRIVER}}", &cache_driver.to_string())
-                .replace("{{CACHE_URI}}", &cache_uri)
-                .replace("{{STRATEGY}}", &strategy.to_string());
-
-            println!();
-            match fs::write("anzar.yml", config_content) {
-                Ok(_) => println!("{} {}", "✓ Created".green().bold(), "anzar.yml".cyan()),
-                Err(e) => eprintln!("{} {}", "✗ Failed to create file:".red().bold(), e),
-            }
+        if !confirmation {
+            return;
         }
+    }
+
+    let (db_driver, db_uri) = select_database();
+    let (cache_driver, cache_uri) = select_cache();
+    let strategy = select_strategy();
+
+    let auth = if strategy == AuthStrategy::Jwt {
+        JWT_AUTH
+    } else {
+        SESSION_AUTH
+    };
+
+    let config_content = CONFIG_TEMPLATE
+        .replace("{{DATABASE_DRIVER}}", &db_driver.to_string())
+        .replace("{{DATABASE_URI}}", &db_uri)
+        .replace("{{CACHE_DRIVER}}", &cache_driver.to_string())
+        .replace("{{CACHE_URI}}", &cache_uri)
+        .replace("{{STRATEGY}}", &strategy.to_string())
+        .replace("{{AUTH}}", auth);
+
+    println!();
+    match fs::write("anzar.yml", config_content) {
+        Ok(_) => println!("{} {}", "✓ Created".green().bold(), "anzar.yml".cyan()),
+        Err(e) => eprintln!("{} {}", "✗ Failed to create file:".red().bold(), e),
     }
 }
